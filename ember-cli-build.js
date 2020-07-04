c9e7bd0e4de66c30f946e34864ce1386eeaa1f85
chenweiyan <chanweiyan007@gmail.com>
2020-07-04 17:00:39 +0800

    import jquery and bootstrap

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,8 +3,16 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
+
+  var bootstrapPath = 'bower_components/bootstrap-sass/assets/';
+  var jqueryPath = 'bower_components/jquery/dist/';
+
   let app = new EmberApp(defaults, {
-    // Add options here
+    sassOptions: {
+      includePaths: [
+        bootstrapPath + 'stylesheets'
+      ]
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -19,6 +27,11 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+
+  // 生成指向bootstrap资源文件的路径
+  // 使用import向应用添加对该资源的引用
+  app.import(jqueryPath + 'jquery.js');
+  app.import(bootstrapPath + 'javascripts/bootstrap.js');
 
   return app.toTree();
 };
